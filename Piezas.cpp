d8689e4ac9c7efc78200d9027da7768185e505ff
Asterisk007 <spykid1998@gmail.com>
2020-05-07 13:02:30 -0700

    Correct for loop int being incremented

--- Piezas.cpp
+++ Piezas.cpp
@@ -1,6 +1,5 @@
 #include "Piezas.h"
 #include <vector>
-#include <iostream>
 /** CLASS Piezas
  * Class for representing a Piezas vertical board, which is roughly based
  * on the game "Connect Four" where pieces are placed in a column and 
@@ -123,7 +122,7 @@ Piece Piezas::gameState()
 {
     // Check that the board has been filled
     for (int i = 0; i < (int)board.size(); i++){
-        for (int j = 0; j < (int)board[i].size(); i++){
+        for (int j = 0; j < (int)board[i].size(); j++){ 
             // If not, return invalid.
             if (board[i][j] == Blank)
                 return Invalid;
@@ -155,14 +154,12 @@ Piece Piezas::gameState()
     // 3x1
     for (int i = 0; i < 3; i++){
         if ( (board[i][0] == board[i][1]) && (board[i][1] == board[i][2]) && (board[i][2] != board[i][3]) ){
-            std::cerr << "Segfault happening in 3x1 check, first if" << std::endl;
             if (board[i][1] == X){
                 player_X_lines += 1;
             } else {
                 player_O_lines += 1;
             }
         } else if ( (board[i][0] != board[i][1]) && (board[i][1] == board[i][2]) && (board[i][2] == board[i][3]) ){
-            std::cerr << "Segfault happening in 3x1 check, second if" << std::endl;
             if (board[i][1] == X){
                 player_X_lines += 1;
             } else {
@@ -173,7 +170,6 @@ Piece Piezas::gameState()
     // 4x1
     for (int i = 0; i < 3; i++){
         if ( (board[i][0] == board[i][1]) && (board[i][1] == board[i][2]) && (board[i][2] == board[i][3]) ){
-            std::cerr << "Segfault happening in 4x1 check" << std::endl;
             if (board[i][0] == X){
                 player_X_lines += 1;
             } else {
